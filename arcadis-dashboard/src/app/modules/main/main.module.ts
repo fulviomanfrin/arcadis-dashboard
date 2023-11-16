03a9cfde5b2fa220ab8b3ff2b6fc27da4f23487d
Fulvio Manfrin <manfrin.fulvio@gmail.com>
2023-11-15 23:55:23 -0300

    Add fleet overview

--- arcadis-dashboard/src/app/modules/main/main.module.ts
+++ arcadis-dashboard/src/app/modules/main/main.module.ts
@@ -5,21 +5,28 @@ import {MenubarModule} from "primeng/menubar";
 import {MenuModule} from "primeng/menu";
 import {FormsModule} from "@angular/forms";
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { BodyComponent } from './dashboard/body/body.component';
+import {CardModule} from "primeng/card";
+import { FleetOverviewComponent } from './dashboard/body/components/fleet-overview/fleet-overview.component';
 
 
 @NgModule({
   declarations: [
 
-    TopBarComponent
+    TopBarComponent,
+     BodyComponent,
+     FleetOverviewComponent
   ],
   exports: [
-    TopBarComponent
+    TopBarComponent,
+    BodyComponent
   ],
   imports: [
     CommonModule,
     MenubarModule,
     MenuModule,
-    FormsModule
+    FormsModule,
+    CardModule
   ]
 })
 export class MainModule { }
